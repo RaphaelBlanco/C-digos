66a79f5e89bd079605a3dc1aeaebfba19cabdd2d
Raphael Zanin Blanco <104874840+RaphaelBlanco@users.noreply.github.com>
2022-07-22 14:30:44 -0300

    Add files via upload

---
+++ Banco/Conta.cs
@@ -0,0 +1,50 @@
+﻿
+namespace Banco
+{
+    class Conta
+    {
+        private string _cliente;
+        public double Saldo { get; private set; }
+        public int NumeroConta { get; private set; }
+
+        public string Cliente
+        {
+            get
+            {
+                return _cliente;
+            }
+            set
+            {
+                if (value != null && value.Length > 1)
+                    _cliente = value;
+            }
+        }
+
+        public Conta(string cliente, int numeroConta)
+        {
+            _cliente = cliente;
+            NumeroConta = numeroConta;
+        }
+
+        public void Deposito(string deposito, double saldo)
+        {
+            if (deposito == "s")
+            {
+                Saldo = Saldo + saldo;
+            }
+        }
+
+        public void Saque(string deposito, double saque)
+        {
+            if (deposito == "s")
+            {
+                Saldo = Saldo - saque - 5.00;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Nome do cliente: " + _cliente + ", numero da conta: " + NumeroConta + ", saldo R$" + Saldo.ToString("f2");
+        }
+    }
+}
---
+++ Banco/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace Banco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Digite o seu nome: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Digite o numero da Conta: ");
+            int numeroConta = int.Parse(Console.ReadLine());
+
+            Conta c = new Conta(nome, numeroConta);
+
+            Console.WriteLine("Gostaria de realizar seu primeiro deposito? (s/n)");
+            string resposta = Console.ReadLine();
+            if (resposta == "s")
+            {
+                Console.WriteLine("Digite o valor do deposito R$");
+                double saldo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                c.Deposito(resposta, saldo);
+            }
+            Console.WriteLine(c);
+
+            Console.WriteLine("Realizar um saque? (s/n)");
+            string respostaSaque = Console.ReadLine();
+            if (respostaSaque == "s")
+            {
+                Console.WriteLine("Digite o valor do saque R$");
+                double saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                c.Saque(respostaSaque, saque);
+            }
+            Console.WriteLine(c);
+        }
+    }
+}
